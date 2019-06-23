e81c22d4a9355a88a48167aa0ba4b906f94395c0
mymusic56 <961272668@qq.com>
2019-06-23 11:22:20 +0800

    加入路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import routers from './router.js'
+
+Vue.use(VueRouter)
+
+var router = new VueRouter({
+  routes: routers
+})
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,8 @@
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './Test'
+const routes = [
+    {path: '/gohome', component: HelloWorld},
+    {path: '/gotest', component: Test}
+];
+
+export default routes;
